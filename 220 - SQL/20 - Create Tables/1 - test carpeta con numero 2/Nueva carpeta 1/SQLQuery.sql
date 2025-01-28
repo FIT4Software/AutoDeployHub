0f7a3576d5a6ae2f898ac00c081ac6d6849b2bc0
silvarodrigo2590 <silvarodrigo2590@gmail.com>
2025-01-28 09:52:42 -0300

    test check

---
+++ 220 - SQL/20 - Create Tables/1 - test carpeta con numero 2/Nueva carpeta 1/SQLQuery.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO [dbo].[AppVersions]
+           ([App_Name]
+           ,[App_Version]
+           ,[Modified_On])
+     VALUES
+           ('test app no va'
+           ,'1.1'
+           ,GETDATE())
+GO
+
